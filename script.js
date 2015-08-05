91cd36c93e187c2eb32115a4d6e3082b3fa55eef
bigfish10 <pressaccob@gmail.com>
2015-08-04 21:55:34 -0300

    tp final parte 1

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+	$('.one-time').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true
+
+	});
+	var titulo = document.getElementsByTagName("a");
+	var imagenes=document.getElementsByTagName("img");
 
 
+	$(titulo).on("mouseenter", function() {
+		$(this).css("opacity", "0");
+		$(this).parent().find("img").css("opacity", '1');
+
+	});
+
+	$(titulo).on("mouseleave", function() {
+		$(this).css("opacity", "1");
+		$(this).parent().find("img").css("opacity", '0.5');				
+
+	});
 });
